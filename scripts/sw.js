1d4c7b77bfa24445bfe59622dd2f211aa7d3b346
Rich Eales <rich@medicodigital.co.uk>
2018-03-14 10:29:53 +0000

    Adding script 'sw.js'

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,13 +1,13 @@
-importScripts('/cache-polyfill.js');
-self.addEventListener('install',function(e){
-  e.waitUntil(caches.open('airhorner').then(function(cache){
-    return cache.addAll([
-      '/',
-      '/css/critical.css',
-      '/css/bootstrap4.css',
-      '/scripts/site.js',
-      '/media/1048/circle-health.png',
-      '/media/1047/medical-website-design.png'
-    ]);
-  }));
-});
+// importScripts('/cache-polyfill.js');
+// self.addEventListener('install',function(e){
+//   e.waitUntil(caches.open('airhorner').then(function(cache){
+//     return cache.addAll([
+//       '/',
+//       '/css/critical.css',
+//       '/css/bootstrap4.css',
+//       '/scripts/site.js',
+//       '/media/1048/circle-health.png',
+//       '/media/1047/medical-website-design.png'
+//     ]);
+//   }));
+// });
